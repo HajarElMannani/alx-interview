3335f64be2c3f8f17c9297847d54af44bf036c8d
HajarElMannani <laso125@hotmail.com>
2025-05-29 11:05:28 -0700

    Semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,34 +2,34 @@
 const request = require('request');
 const id = process.argv[2];
 if (!id) {
-    process.exit(1);
+  process.exit(1);
 }
-const furl = 'https://swapi-api.alx-tools.com/${id}';
-request(furl, {json: true}, (err, res, body) => {
-    if (err) {
-	return;
+const furl = `https://swapi-api.alx-tools.com/api/films/${id}/`;
+request(furl, { json: true }, (err, res, body) => {
+  if (err) {
+    return;
+  }
+  if (res.statusCode !== 200) {
+    return;
+  }
+  const charact = body.characters;
+  if (!charact || !charact.length) {
+    return;
+  }
+  (async () => {
+    for (const url of charact) {
+      await new Promise((resolve, reject) => {
+        request(url, { json: true }, (err, res, body) => {
+          if (err) {
+            return resolve();
+          }
+          if (res.statusCode !== 200) {
+            return resolve();
+          }
+          console.log(body.name);
+          resolve();
+        });
+      });
     }
-    if(res.statusCode != 200) {
-	return;
-    }
-    const charact = body.characters;
-    if (!charact || !charact.length) {
-	return;
-    }
-    (async () => {
-	for (const url of charact) {
-	    await new Promise((resolve, reject) => {
-		request(url, {json: true}, (err, res, body) => {
-		    if (err) {
-			return resolve();
-		    }
-		    if (res.statusCode != 200) {
-			return resolve();
-		    }
-		    console.log(body.name);
-		    resolve();
-		});
-	    });
-	}
-    })();
+  })();
 });
