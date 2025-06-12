99e8c56d550d5bf645bbf787df56b9d235ffa682
HajarElMannani <laso125@hotmail.com>
2025-06-12 21:59:40 +0000

    Pycodestyle

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """Function makeChange"""
 
+
 def makeChange(coins, total):
-    """determine the fewest number of coins 
+    """determine the fewest number of coins
     needed to meet a given amount total"""
     if total <= 0:
         return 0
     tbl = [0] + [total + 1] * total
     for i in range(1, total + 1):
         for coin in coins:
-            prec = tbl[i - coin]
-            if prec + 1 < tbl[i]:
-                tbl[i] = prec + 1
+            if coin <= i:
+                prec = tbl[i - coin]
+                if prec + 1 < tbl[i]:
+                    tbl[i] = prec + 1
     if tbl[total] != total + 1:
         return tbl[total]
     else:
